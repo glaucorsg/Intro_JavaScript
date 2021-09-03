2b02afd23b2a223f37c282517dc3f346cddc6726
glaucorsg <glaucorsg@gmail.com>
2021-09-02 23:57:40 -0300

    Cria pasta js e arquivo main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var nome = "Glauco Gomes";
+var n1 = 15;
+var n2 = "10";
+var pot = n1/n2; 
+var cpf = "045.588.484-63"
+var frase = "Nunca compre um Marea "
+//alert(nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log("CPF: " + cpf);
+console.log(n1/n2);
+console.log(frase.toUpperCase() + pot);
+alert(frase.toUpperCase() + pot);
+//alert(frase.replace("Marea", "Peugeot"));
+
+
